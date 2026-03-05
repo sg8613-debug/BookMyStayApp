19c19aef8656252a2e093ca9cbe61405b100b139
sg8613-debug <sg8613@srmist.edu.in>
2026-03-05 09:14:50 +0530

    Shourya Added UC1-Application-Entry

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,27 @@
+/**
+ * Hotel Booking Application
+ * This class represents the entry point of the Hotel Booking System.
+ * It starts the application and displays a welcome message.
+ *
+ * @author Shourya
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    /**
+     * Main method - Entry point of the Java application.
+     * JVM starts program execution from here.
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to Hotel Booking System");
+
+        // Display application name and version
+        System.out.println("Hotel Booking System v1.0");
+
+        // Inform user that application has started
+        System.out.println("Application started successfully.");
+    }
+}
