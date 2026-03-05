70bc3d16e8b8069b125c7e46db367896dc012c45
sg8613-debug <sg8613@srmist.edu.in>
2026-03-05 09:19:55 +0530

    Shourya Added feature/UC2-RoomTypes

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -7,21 +7,81 @@
  * @version 1.0
  */
 
-public class HotelBookingApp {
+/**
+ * BookMyStayApp
+ * Demonstrates Room Types and Static Availability
+ *
+ * @author Shourya
+ * @version 2.1
+ */
 
-    /**
-     * Main method - Entry point of the Java application.
-     * JVM starts program execution from here.
-     */
+abstract class Room {
+
+    String roomType;
+    int beds;
+    int size;
+    double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 3000);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 5000);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 600, 9000);
+    }
+}
+
+public class BookMyStayApp {
+
     public static void main(String[] args) {
 
-        // Display welcome message
-        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("Welcome to BookMyStayApp v2.1");
+        System.out.println("------------------------------");
 
-        // Display application name and version
-        System.out.println("Hotel Booking System v1.0");
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        // Inform user that application has started
-        System.out.println("Application started successfully.");
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailable);
     }
 }
